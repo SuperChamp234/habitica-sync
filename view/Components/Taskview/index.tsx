9c58769a99305e34a2d38a2175f8442a3dc39996
Leoh <leonardh@tuta.io>
2021-10-16 23:33:38 +0530

    CSS and Tabs Added

--- view/Components/Taskview/index.tsx
+++ view/Components/Taskview/index.tsx
@@ -1,9 +1,30 @@
 import * as React from "react";
 import TodoItem from "./TodoItem"
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 export default function Index(props: any){
-    const listItems = props.todos.map((todo: any) => {
+    const incompleteTodos = props.todos.map((todo: any) => {
+            if(!todo.completed)
+                return <TodoItem key={todo.id} id={todo.id} todo_text={todo.text} onChange={props.onChange} completed={todo.completed}/>
+    })
+    const completedTodos = props.todos.map((todo: any) => {
+        if(todo.completed)
             return <TodoItem key={todo.id} id={todo.id} todo_text={todo.text} onChange={props.onChange} completed={todo.completed}/>
     })
-    return(<ul>{listItems}</ul>);
+    const display = <div id="classDisplay">
+                        <Tabs>
+                        <TabList>
+                            <Tab>Active Todos</Tab>
+                            <Tab>Completed Todos</Tab>
+                        </TabList>
+                        <TabPanel>
+                            <ul>{incompleteTodos}</ul>
+                        </TabPanel>
+                        <TabPanel>
+                            <ul>{completedTodos}</ul>
+                        </TabPanel>
+                        </Tabs>
+                    </div>
+
+    return(display);
 }
