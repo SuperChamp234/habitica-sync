32f0e6bc989f3f125cde77ae0986dbb34ff35c4c
Leoh <leonardh@tuta.io>
2021-10-17 18:16:52 +0530

    Organized all files. Code goes in src/ Added icon

--- ReactView.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import App from "./view/App";
-
-export default function ReactView(props: any){
-  return(
-    <App username={props.userID} apiToken={props.tokenAPI} plugin={props.plugin}/>
-  )
-}
