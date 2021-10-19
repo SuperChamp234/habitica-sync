25312a6d93b5d20756ea1521704e222063ee8686
Leoh <leonardh@tuta.io>
2021-10-19 19:13:48 +0530

    Cleaned up Taskview Index

--- src/view/Components/Taskview/index.tsx
+++ src/view/Components/Taskview/index.tsx
@@ -5,28 +5,28 @@ import Todoview from "./Todoview"
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 
 export default function Index(props: any){
-    const display = <div id="classDisplay">
+    const display = <div className="taskView">
                     <Tabs>
-                    <TabList>
-                        <Tab>
-                            <span className="material-icons md-24">task_alt</span>
-                        </Tab>
-                        <Tab>
-                            <span className="material-icons">today</span>
-                        </Tab>
-                        <Tab>
-                            <span className="material-icons">add_circle_outline</span>
-                        </Tab>
-                    </TabList>
-                    <TabPanel>
-                        <Habitsview habits={props.data.habits} onChange={props.handleChangeHabits}/>
-                    </TabPanel>
-                    <TabPanel>
-                        <Dailiesview dailys={props.data.dailys} onChange={props.handleChangeDailys} />
-                    </TabPanel>
-                    <TabPanel>
-                    <Todoview todos={props.data.todos} onChange={props.handleChangeTodos} />
-                    </TabPanel>
+                        <TabList>
+                            <Tab>
+                                <span className="material-icons md-24">task_alt</span>
+                            </Tab>
+                            <Tab>
+                                <span className="material-icons">today</span>
+                            </Tab>
+                            <Tab>
+                                <span className="material-icons">add_circle_outline</span>
+                            </Tab>
+                        </TabList>
+                        <TabPanel>
+                            <Habitsview habits={props.data.habits} onChange={props.handleChangeHabits}/>
+                        </TabPanel>
+                        <TabPanel>
+                            <Dailiesview dailys={props.data.dailys} onChange={props.handleChangeDailys} />
+                        </TabPanel>
+                        <TabPanel>
+                        <Todoview todos={props.data.todos} onChange={props.handleChangeTodos} />
+                        </TabPanel>
                     </Tabs>
                 </div>
     return(display);
