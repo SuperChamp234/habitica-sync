3d61b94932e02d6990212e28584aa452e4a2847b
Zain <leonardh@tuta.io>
2021-11-16 18:53:03 +0530

    Added Emoji Support

--- src/view/Components/Taskview/Dailiesview/DailyItem.tsx
+++ src/view/Components/Taskview/Dailiesview/DailyItem.tsx
@@ -1,10 +1,11 @@
- import * as React from "react";
+import Emoji from "react-emoji-render";
+import * as React from "react";
 
 function DailyItem(props: any) {
     return (
         <div className="todo-item" id={props.id}>
             <input type="checkbox" className="checkbox" id={props.id} onChange={props.onChange} checked={props.completed}/>
-            <p>{props.daily_text}</p>
+            <p><Emoji text={props.daily_text}></Emoji></p>
         </div>
     )
 }
